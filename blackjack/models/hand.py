8c57a6fc35d854f2c9d7fd697381e13381169bcd
James Neale <42097394+james9226@users.noreply.github.com>
2024-01-07 19:26:55 +0000

    Encapsulate score logic

--- blackjack/models/hand.py
+++ blackjack/models/hand.py
@@ -1,21 +1,26 @@
-
-
-from blackjack.functions.score import calculate_score
+from blackjack.data.constants import CARD_VALUE_MAP
 
 
 class BlackJackHand:
-
-    def __init__(self, initial_hand : list[int]):
+    def __init__(self, initial_hand: list[int]):
         self.hand = initial_hand
 
     def get_score(self) -> int:
-        return calculate_score(self.hand) 
+        score = sum(CARD_VALUE_MAP.get(card, 0) for card in self.hand)
+
+        num_aces = self.hand.count(1)
+
+        while num_aces > 0 and score > 21:
+            score -= 10
+            num_aces -= 1
+
+        return score
 
     def get_first_card(self) -> int:
-        return self.hand[0] 
+        return self.hand[0]
 
     def has_ace(self) -> int:
-        return int(1 in self.hand) 
+        return int(1 in self.hand)
 
     def is_charlie(self) -> bool:
         return len(self.hand) >= 7
@@ -23,5 +28,5 @@ class BlackJackHand:
     def is_bust(self) -> bool:
         return self.get_score() > 21
 
-    def add_card(self, card : int) -> None:
+    def add_card(self, card: int) -> None:
         self.hand.append(card)
