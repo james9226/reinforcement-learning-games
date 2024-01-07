143de44d095ce738a477c6cc0e1792406a83971c
James Neale <42097394+james9226@users.noreply.github.com>
2024-01-07 19:21:44 +0000

    Cache CI + refactor score

--- blackjack/functions/score.py
+++ blackjack/functions/score.py
@@ -4,12 +4,10 @@ from blackjack.data.constants import CARD_VALUE_MAP
 def calculate_score(hand: list[int]) -> int:
     score = sum(CARD_VALUE_MAP.get(card, 0) for card in hand)
 
-    num_aces = sum(1 for card in hand if card == 1)
+    num_aces = hand.count(1)
 
-    if score <= 21:
-        return score
-
-    while score > 21 and num_aces > 0:
-        num_aces -= 1
+    while num_aces > 0 and score > 21:
         score -= 10
+        num_aces -= 1
+
     return score
