47b0995c03cfc8066e6dd98959e5fe6a6732b62b
James Neale <42097394+james9226@users.noreply.github.com>
2024-01-07 19:15:07 +0000

    Type checking + CI fix

--- blackjack/functions/score.py
+++ blackjack/functions/score.py
@@ -1,17 +1,15 @@
-
-
 from blackjack.data.constants import CARD_VALUE_MAP
 
 
-def calculate_score(hand : list[int]) -> int:
-    score = sum(CARD_VALUE_MAP.get(card) for card in hand)
+def calculate_score(hand: list[int]) -> int:
+    score = sum(CARD_VALUE_MAP.get(card, 0) for card in hand)
 
     num_aces = sum(1 for card in hand if card == 1)
 
     if score <= 21:
-        return score 
-    
+        return score
+
     while score > 21 and num_aces > 0:
         num_aces -= 1
-        score -= 10 
-    return score 
+        score -= 10
+    return score
